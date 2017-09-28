2b0e34a293c3d85bee8ba0fdf19370a1a258079f
Peter Hunt <peter.s.hunt@gmail.com>
2017-09-28 16:03:00 +0000

    edit login route pre cookie-session install

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ const knexLogger  = require('knex-logger');
 // Seperated Routes for each Resource
 const todosRoutes = require("./routes/todos");
 
+
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
 //         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
@@ -35,8 +36,26 @@ app.use("/styles", sass({
 }));
 app.use(express.static("public"));
 
+// Need to pass locals...?
+// app.use(function (req, res, next) {
+//   const userID = req.params.id
+//   res.locals = {
+//     user: userID
+//   };
+//   next();
+// });
+
+
 // Mount all resource routes
 app.use("/todos", todosRoutes(knex));
+
+//login route
+app.get("/user/:id/login/", (req, res) => {
+  let user_id = req.params.id;
+  let templateVars = { user: user_id }
+  req.redirect('/');
+});
+
 
 // Home page
 app.get("/", (req, res) => {
