c4fc285d4791f17affcb879da7f68be54438886f
tasha-urbancic <tasha.urbancic@gmail.com>
2017-09-28 19:19:01 +0000

    required queries into todos route

--- queries/queries.js
+++ queries/queries.js
@@ -1,20 +1,23 @@
-
 module.exports = {
-
-  getUser : function (knex, userName) {
-    knex.select().from('users').where({email: userName}).asCallback((error, rows) => {
-      //console.log(rows);
-      return rows;
-    })
+  getUser: function(knex, userName) {
+    knex
+      .select()
+      .from("users")
+      .where({ email: userName })
+      .asCallback((error, rows) => {
+        return rows;
+      });
   },
 
-  getTodoList : function (knex, userid) {
-    knex.select().from('todos').where({user_id: userid}).asCallback((error, rows) => {
-      //console.log(rows);
-      return rows;
-    })
+  getTodoList: function(knex, req, res) {
+    knex
+      .select("*")
+      .from("todos")
+      .where("user_id", req.session.user_id)
+      .orderBy("category_id", "asc")
+      .orderBy("created_at", "desc")
+      .then(results => {
+        res.json(results);
+      });
   }
-
-
-
-}
+};
--- routes/todos.js
+++ routes/todos.js
@@ -2,19 +2,11 @@
 
 const express = require("express");
 const router = express.Router();
+const queries = require('../queries/queries');
 
 module.exports = knex => {
   router.get("/", (req, res) => {
-
-    knex
-      .select("*")
-      .from("todos")
-      .where("user_id", req.session.user_id)
-      .orderBy("category_id", 'asc')
-      .orderBy("created_at", 'desc')
-      .then(results => {
-        res.json(results);
-      });
+    queries.getTodoList(knex, req, res);
   });
 
   return router;
