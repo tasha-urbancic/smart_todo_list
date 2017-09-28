737dbec793b0e6d5f934bb2cb15b98777f424909
Peter Hunt <peter.s.hunt@gmail.com>
2017-09-28 02:22:55 +0000

    add keywords to seed file

--- db/seeds/initial.js
+++ db/seeds/initial.js
@@ -12,48 +12,83 @@
 //     });
 // };
 
-exports.seed = function (knex, Promise) {
+exports.seed = function(knex, Promise) {
   return Promise.all([
-    knex('categories').returning('id').insert([{
-      name: 'media'
-    }, {
-      name: 'books'
-    }, {
-      name: 'food'
-    }, {
-      name: 'products'
-    }]),
-    knex('users').returning('id').insert({
-      email: '1@1.com',
-      password_hash: 'asdfasdf'
-    })
+    knex("categories")
+      .returning("id")
+      .insert([
+        {
+          name: "media"
+        },
+        {
+          name: "books"
+        },
+        {
+          name: "food"
+        },
+        {
+          name: "products"
+        }
+      ]),
+    knex("users")
+      .returning("id")
+      .insert({
+        email: "1@1.com",
+        password_hash: "asdfasdf"
+      })
   ]).then(([categoryIds, userIds]) => {
-    return knex('todos').insert([{
-      user_id: userIds[0],
-      category_id: categoryIds[0],
-      item: 'Watch Gladiator',
-      completed_toggle: 0,
-    }, {
-      user_id: userIds[0],
-      category_id: categoryIds[1],
-      item: 'Read Time Magazine',
-      completed_toggle: 0,
-    }, {
-      user_id: userIds[0],
-      category_id: categoryIds[3],
-      item: 'Buy Garden Trowel',
-      completed_toggle: 0,
-    }, {
-      user_id: userIds[0],
-      category_id: categoryIds[1],
-      item: 'Read Programming 101 for Bootcamps',
-      completed_toggle: 0,
-    }, {
-      user_id: userIds[0],
-      category_id: categoryIds[2],
-      item: 'Eat at Tacofino!',
-      completed_toggle: 1,
-    }
-    ]);
+    return knex("todos")
+      .insert([
+        {
+          user_id: userIds[0],
+          category_id: categoryIds[0],
+          item: "Watch Gladiator",
+          completed_toggle: 0
+        },
+        {
+          user_id: userIds[0],
+          category_id: categoryIds[1],
+          item: "Read Time Magazine",
+          completed_toggle: 0
+        },
+        {
+          user_id: userIds[0],
+          category_id: categoryIds[3],
+          item: "Buy Garden Trowel",
+          completed_toggle: 0
+        },
+        {
+          user_id: userIds[0],
+          category_id: categoryIds[1],
+          item: "Read Programming 101 for Bootcamps",
+          completed_toggle: 0
+        },
+        {
+          user_id: userIds[0],
+          category_id: categoryIds[2],
+          item: "Eat at Tacofino!",
+          completed_toggle: 1
+        }
+      ])
+      .then(() => {
+        return knex("keywords").insert([
+          {
+            keyword: "watch",
+            category_id: categoryIds[0]
+          },
+          {
+            keyword: "read",
+            category_id: categoryIds[1]
+          },
+          {
+            keyword: "buy",
+            category_id: categoryIds[3]
+          },
+          {
+            keyword: "eat",
+            category_id: categoryIds[2]
+          }
+        ]);
+      });
   });
 };
