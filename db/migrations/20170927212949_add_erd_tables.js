44d613baf509d073675906d62d15a286ec738f64
Peter Hunt <peter.s.hunt@gmail.com>
2017-09-28 00:02:15 +0000

    add increments to ids for 3 tables

--- db/migrations/20170927212949_add_erd_tables.js
+++ db/migrations/20170927212949_add_erd_tables.js
@@ -9,12 +9,12 @@ exports.up = function(knex, Promise) {
     }),
 
     knex.schema.createTable('categories', function(table) {
-      table.integer('id').notNullable().primary();
+      table.increments().notNullable().primary();
       table.string('name').notNullable();
     }),
 
     knex.schema.createTable('todos', function(table) {
-      table.integer('id').notNullable().primary()
+      table.increments().notNullable().primary()
       table.string('item').notNullable();
       table.boolean('completed_toggle').notNullable();
       table.integer('user_id').notNullable();
@@ -23,7 +23,7 @@ exports.up = function(knex, Promise) {
     }),
 
     knex.schema.createTable('keywords', function(table) {
-      table.integer('id').notNullable().primary();
+      table.increments().notNullable().primary();
       table.string('keyword').notNullable();
       table.integer('category_id').notNullable();
     })
