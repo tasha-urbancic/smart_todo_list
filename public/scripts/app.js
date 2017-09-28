3d9cf6374bcca2b660260e0b726e8e4d64844eb6
tasha-urbancic <tasha.urbancic@gmail.com>
2017-09-28 19:10:19 +0000

    added to todos get route to filter by user id

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,8 +5,7 @@ $(() => {
   }).done(todos => {
     for (var i = 0; i < todos.length; i++) {
       var categoryId = todos[i].category_id;
-      console.log(req.session.user_id);
-
+  
       $("<li>")
         .text(todos[i].item)
         .appendTo($("ul[data-category=" + categoryId + "]"));
--- routes/todos.js
+++ routes/todos.js
@@ -5,9 +5,11 @@ const router = express.Router();
 
 module.exports = knex => {
   router.get("/", (req, res) => {
+
     knex
       .select("*")
       .from("todos")
+      .where("user_id", req.session.user_id)
       .orderBy("category_id", 'asc')
       .orderBy("created_at", 'desc')
       .then(results => {
