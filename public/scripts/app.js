99e3cca16c8e54b6b75d7c5c6b3f2f81b4a15c96
tasha-urbancic <tasha.urbancic@gmail.com>
2017-09-29 18:09:14 +0000

    added selective coloring to category circles based on current list

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,3 +1,59 @@
+function createCategoryCircles(catId) {
+
+  var keyNum = $("ul[data-category=" + catId + "]").data('key');
+
+  const circle1 = $("<i>")
+    .addClass("cat-color-0")
+    .attr("aria-hidden", "true");
+  const circle2 = $("<i>")
+    .addClass("cat-color-1")
+    .attr("aria-hidden", "true");
+  const circle3 = $("<i>")
+    .addClass("cat-color-2")
+    .attr("aria-hidden", "true");
+  const circle4 = $("<i>")
+    .addClass("cat-color-3")
+    .attr("aria-hidden", "true");
+
+  if (keyNum === 0) {
+    circle1.addClass("fa fa-circle").addClass("current-category");
+    circle2.addClass("fa fa-circle-o").addClass("alternate-category");
+    circle3.addClass("fa fa-circle-o").addClass("alternate-category");
+    circle4.addClass("fa fa-circle-o").addClass("alternate-category");
+  }
+
+  if (keyNum === 1) {
+    circle1.addClass("fa fa-circle-o").addClass("alternate-category");
+    circle2.addClass("fa fa-circle").addClass("current-category");
+    circle3.addClass("fa fa-circle-o").addClass("alternate-category");
+    circle4.addClass("fa fa-circle-o").addClass("alternate-category");
+  }
+
+  if (keyNum === 2) {
+    circle1.addClass("fa fa-circle-o").addClass("alternate-category");
+    circle2.addClass("fa fa-circle-o").addClass("alternate-category");
+    circle3.addClass("fa fa-circle").addClass("current-category");
+    circle4.addClass("fa fa-circle-o").addClass("alternate-category");
+  }
+
+  if (keyNum === 3) {
+    circle1.addClass("fa fa-circle-o").addClass("alternate-category");
+    circle2.addClass("fa fa-circle-o").addClass("alternate-category");
+    circle3.addClass("fa fa-circle-o").addClass("alternate-category");
+    circle4.addClass("fa fa-circle").addClass("current-category");
+  }
+
+  const categoryDiv = $("<div>")
+    .addClass("category-button")
+    .append(circle1)
+    .append(circle2)
+    .append(circle3)
+    .append(circle4)
+    .css("display", "none");
+
+  return categoryDiv;
+}
+
 function createTodo(itemObj) {
   const listItem = $("<li>")
     .addClass("item-wrapper")
@@ -16,35 +72,8 @@ function createTodo(itemObj) {
   const markerDiv = $("<div>")
     .addClass("marker-button")
     .append(marker);
-  const circle = $("<i>")
-    .addClass("fa fa-circle")
-    .attr("aria-hidden", "true");
-  const circleDiv = $("<div>")
-    .addClass("circle-button")
-    .append(circle)
-    .css("display", "none");
 
-  const circle1 = $("<i>")
-      .addClass("fa fa-circle-o")
-      .addClass("fa fa-circle")
-      .attr("aria-hidden", "true");
-  const circle2 = $("<i>")
-      .addClass("fa fa-circle")
-      .attr("aria-hidden", "true");
-  const circle3 = $("<i>")
-      .addClass("fa fa-circle")
-      .attr("aria-hidden", "true");
-  const circle4 = $("<i>")
-      .addClass("fa fa-circle")
-      .attr("aria-hidden", "true");
-  const categoryDiv = $("<div>")
-      .addClass("category-button")
-      .append(circle1)
-      .append(circle2)
-      .append(circle3)
-      .append(circle4)
-      .css("display", "none");
-
+  var categoryDiv = createCategoryCircles(itemObj.categoryId);
 
   listItem
     .append(markerDiv)
@@ -55,12 +84,10 @@ function createTodo(itemObj) {
         .text(itemObj.text)
     )
     .append(trashDiv)
-    .append(circleDiv)
     .append(categoryDiv);
 
   listItem.prependTo($("ul[data-category=" + itemObj.categoryId + "]"));
 }
-
 
 $(() => {
   function getTodos() {
@@ -112,10 +139,6 @@ $(() => {
       });
   }
 
-
-
-
-
   function updateTodo(text, itemId) {
     // let text = $(".todo-post-box").val();
     // const itemId = $(event.target).closest('li').data('id');
@@ -123,30 +146,33 @@ $(() => {
     console.log(itemId);
   }
 
-
-
-  $('body').on('focus', '[contenteditable]', function() {
-    var $this = $(this);
-    $this.data('before', $this.html());
-    //console.log($this);
-    return $this;
-  }).on('blur keyup paste input', '[contenteditable]', function() {
-    var $this = $(this);
-    if ($this.data('before') !== $this.html()) {
-        $this.data('before', $this.html());
-        $this.trigger('change');
-    }
-    //console.log($this);
-    return $this;
-  }).on('keydown', function(e) {
+  $("body")
+    .on("focus", "[contenteditable]", function() {
+      var $this = $(this);
+      $this.data("before", $this.html());
+      //console.log($this);
+      return $this;
+    })
+    .on("blur keyup paste input", "[contenteditable]", function() {
+      var $this = $(this);
+      if ($this.data("before") !== $this.html()) {
+        $this.data("before", $this.html());
+        $this.trigger("change");
+      }
+      //console.log($this);
+      return $this;
+    })
+    .on("keydown", function(e) {
       if (e.keyCode === 13) {
         e.preventDefault();
         let text = $this;
         //console.log(text);
-        const itemId = $(event.target).closest('li').data('id');
-        updateTodo(text, itemId)
+        const itemId = $(event.target)
+          .closest("li")
+          .data("id");
+        updateTodo(text, itemId);
       }
-  });
+    });
 
   // .on('blur keyup paste input', '[contenteditable]', function() {
   //   var $this = $(this);
@@ -156,7 +182,6 @@ $(() => {
   //   }
   //   return $this;
   // });
-
 
   // $(".item-wrapper").on("keypress", function(event) {
   //   console.log('go');
@@ -177,21 +202,27 @@ $(() => {
   //   })
   // });
 
-
-  $(".todo-list").on("click", 'li', function(event) {
-    $(event.target).siblings('.delete-button').show();
-    $(event.target).siblings('.category-button').show();
-    $()
+  $(".todo-list").on("click", "li", function(event) {
+    $(event.target)
+      .siblings(".delete-button")
+      .show();
+    $(event.target)
+      .siblings(".category-button")
+      .show();
+    $();
   });
 
-
   $(".todo-list").on("click", ".circle-button", function(event) {
-    let $this = $(event.target).parent().parert();//('.category-button');//.toggle();
-    console.log($this);//const itemId = $(event.target).closest('li').data('id');
+    let $this = $(event.target)
+      .parent()
+      .parert(); //('.category-button');//.toggle();
+    console.log($this); //const itemId = $(event.target).closest('li').data('id');
   });
 
   $(".todo-list").on("click", ".fa-trash-o", function(event) {
-    const itemId = $(event.target).closest('li').data('id');
+    const itemId = $(event.target)
+      .closest("li")
+      .data("id");
     event.preventDefault();
     $.ajax({
       method: "POST",
@@ -201,18 +232,20 @@ $(() => {
         console.log(res);
       }
     })
-    .done(function(result) {
-      $(event.target).closest('li').remove();
-    })
-    .fail(function(err) {
-      console.log(err);
-    });
+      .done(function(result) {
+        $(event.target)
+          .closest("li")
+          .remove();
+      })
+      .fail(function(err) {
+        console.log(err);
+      });
   });
 
   $(".todo-button").on("click", createNewTodo);
 
   $(".todo-post-box").on("keypress", function(event) {
-    console.log('run');
+    console.log("run");
     if (event.which === 13) {
       createNewTodo(event);
     }
