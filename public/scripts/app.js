fcf61d032454ee1ffd2f5a441ba8f2883abcb195
tasha-urbancic <tasha.urbancic@gmail.com>
2017-09-29 00:03:28 +0000

    changed styling of main page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,38 +8,91 @@ $(() => {
 
   function loadTodos() {
     return getTodos().done(todos => {
-
       for (var i = 0; i < todos.length; i++) {
         var categoryId = todos[i].category_id;
 
-        $("<li>")
-          .text(todos[i].item)
-          .appendTo($("ul[data-category=" + categoryId + "]"));
+        const listItem = $("<li>").addClass("item-wrapper");
+        listItem
+          .append(
+            $("<i>")
+              .addClass("fa fa-circle-o")
+              .addClass("completed-button")
+              .attr("aria-hidden", "true")
+          )
+          .append(
+            $("<span>")
+              .addClass("item-text")
+              .text(todos[i].item)
+          )
+          .append(
+            $("<i>")
+              .addClass("fa fa-trash-o")
+              .addClass("delete-button")
+              .attr("aria-hidden", "true")
+          )
+          .append(
+            $("<i>")
+              .addClass("fa fa-circle")
+              .addClass("edit-button")
+              .attr("aria-hidden", "true")
+          );
+
+        listItem.prependTo($("ul[data-category=" + categoryId + "]"));
       }
     });
   }
 
   loadTodos();
 
+  // <i class="fa fa-check-circle-o" aria-hidden="true"></i>
+
   function createNewTodo(event) {
-    
     var text = $(".todo-post-box").val();
     var categoryId = 1;
-    $("<li>")
-      .text(text)
-      .prependTo($("ul[data-category=" + categoryId + "]"));
+
+    const listItem = $("<li>").addClass("item-wrapper");
+    listItem
+      .append(
+        $("<i>")
+          .addClass("fa fa-circle-o")
+          .addClass("completed-button")
+          .attr("aria-hidden", "true")
+      )
+      .append(
+        $("<span>")
+          .addClass("item-text")
+          .text(text)
+      )
+      .append(
+        $("<i>")
+          .addClass("fa fa-trash-o")
+          .addClass("delete-button")
+          .attr("aria-hidden", "true")
+      )
+      .append(
+        $("<i>")
+          .addClass("fa fa-circle")
+          .addClass("edit-button")
+          .attr("aria-hidden", "true")
+      );
+
+    listItem.prependTo($("ul[data-category=" + categoryId + "]"));
 
     console.log(text);
     event.preventDefault();
     $.ajax({
       method: "POST",
       url: "/",
-      data: {text: text}
-    }).done(function() {
-      $(".todo-post-box").val('');
-    }).fail( function (err) {
-      $("ul[data-category=" + categoryId + "]").first().remove();
-    });
+      data: { text: text }
+    })
+      .done(function() {
+        $(".todo-post-box").val("");
+      })
+      .fail(function(err) {
+        $("ul[data-category=" + categoryId + "]")
+          .first()
+          .remove();
+      });
   }
 
   $(".todo-button").on("click", createNewTodo);
@@ -49,5 +102,4 @@ $(() => {
       createNewTodo(event);
     }
   });
-
 });
