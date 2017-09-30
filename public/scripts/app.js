a9dd9a27e7b75afa7e4009dd79a8006dffc7386e
Peter Hunt <peter.s.hunt@gmail.com>
2017-09-30 06:04:59 +0000

    remove margin and add replace with less padding for lists

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -283,7 +283,25 @@ $(() => {
     }
   });
 
-   $(".category-header").on('click', function(event) {
+  $(".category-header").on('click', function(event) {
        $(event.target).parent().siblings('.toggle-list').toggle('hide');
   });
+
+  $(".login-button").click(function(e) {
+    e.preventDefault();
+    $.ajax({
+        type: "POST",
+        url: "/pages/test/",
+        data: {
+            id: $(this).val(), // < note use of 'this' here
+            access_token: $("#access_token").val()
+        },
+        success: function(result) {
+            alert('ok');
+        },
+        error: function(result) {
+            alert('error');
+        }
+    });
+});
 });
